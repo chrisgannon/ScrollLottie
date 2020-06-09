a228ea8e8a656b1d841a626563d92c4186c23af7
Chris Gannon <chris@gannon.tv>
2020-06-09 11:39:53 +0100

    Added ScrollLottie.js

---
+++ ScrollLottie.js
@@ -0,0 +1,35 @@
+const ScrollLottie = (obj) => {
+
+  let anim = lottie.loadAnimation({
+   container: document.querySelector(obj.target), 
+   renderer: 'svg',
+   loop: false,
+   autoplay: false,
+   path: obj.path 
+ });
+ 
+ let timeObj = {currentFrame: 0}
+ let endString = (obj.speed === "slow") ? "+=2000" : (obj.speed === "medium") ? "+=1000" : "+=500";
+ ScrollTrigger.create({
+   trigger: obj.target,
+     scrub: true,
+     pin: true,
+     start: "top top",
+     end: endString, 
+     onUpdate: self => {
+      if(obj.duration) {
+       gsap.to(timeObj, {
+        duration: obj.duration,
+        currentFrame:(Math.floor(self.progress *  (anim.totalFrames - 1))),
+        onUpdate: () => {
+         anim.goToAndStop(timeObj.currentFrame, true)
+        },
+        ease: 'expo'
+       })
+      } else {
+        anim.goToAndStop(self.progress *  (anim.totalFrames - 1), true)
+      }
+     }
+ });  
+
+}
